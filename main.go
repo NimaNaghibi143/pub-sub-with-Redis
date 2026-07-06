51816dc3def54fed8008f51e248174be2483df95
Nima Naghibi <dude.nimanaghibi@gmail.com>
2026-07-06 14:42:34 +0330

    feat: create store data strcuture and storage constructor

--- main.go
+++ main.go
@@ -6,6 +6,31 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+type Store struct {
+	data map[int]string
+}
+
+func NewStore() *Store {
+	data := map[int]string{
+		1: "let's learn golang hands on!",
+		2: "foo is not bar and bar is not baz",
+		3: "let's play dota2",
+	}
+
+	return &Store{
+		data: data,
+	}
+}
+
+func (s *Store) Get(key int) (string, error) {
+	val, ok := s.data[key]
+	if !ok {
+		return "", fmt.Errorf("key not found: %d", key)
+	}
+
+	return val, nil
+}
+
 func main() {
 	client := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
@@ -13,5 +38,4 @@ func main() {
 		DB:       0,
 	})
 
-	fmt.Println(client)
 }
