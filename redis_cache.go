f4d82fa430d5104fa242f8b8367d13968db8ade1
Nima Naghibi <dude.nimanaghibi@gmail.com>
2026-07-08 00:03:38 +0330

    feat: create redis client

--- redis_cache.go
+++ redis_cache.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/redis/go-redis/v9"
+import (
+	"context"
+	"strconv"
+
+	"github.com/redis/go-redis/v9"
+)
 
 type RedisCache struct {
 	client *redis.Client
+}
+
+func NewRedisCache(c *redis.Client) *RedisCache {
+	return &RedisCache{
+		client: c,
+	}
+}
+
+func (c *RedisCache) Get(key int) (string, error) {
+	ctx := context.Background()
+	intStr := strconv.Itoa(key)
+	return c.client.Get(ctx, intStr).Result()
 }
