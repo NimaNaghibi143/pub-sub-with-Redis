8cdcc7bc19dc1ecb33d8a672dbcc0672bc32ddf3
Nima Naghibi <dude.nimanaghibi@gmail.com>
2026-07-14 17:58:58 +0330

    feat: create pub/sub flow

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
-	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -15,17 +14,23 @@ func main() {
 		DB:       0,
 	})
 
-	ttl := time.Second * 4
-	s := NewStore(NewRedisCache(client, ttl))
-	for q := 0; q < 2; q++ {
-		val, err := s.Get(3)
-		if err != nil {
-			log.Fatal()
+	// ttl := time.Second * 4
+	// s := NewStore(NewRedisCache(client, ttl))
+	// for q := 0; q < 2; q++ {
+	// 	val, err := s.Get(3)
+	// 	if err != nil {
+	// 		log.Fatal()
+	// 	}
+
+	// 	fmt.Println(val)
+
+	// 	time.Sleep(3 * time.Second)
+	// }
+
+	ctx := context.Background()
+	for i := 0; i < 100; i++ {
+		if err := client.Publish(ctx, "coords", i).Err(); err != nil {
+			log.Fatal(err)
 		}
-
-		fmt.Println(val)
-
-		time.Sleep(3 * time.Second)
 	}
-
 }
---
+++ remote/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	ctx := context.Background()
+	sub := client.Subscribe(ctx, "coords")
+
+	for {
+		msg, err := sub.ReceiveMessage(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%+v\n", msg)
+	}
+}
