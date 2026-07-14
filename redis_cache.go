96ed551c61b6f2e9fe77bf7d36324e603f9aecb7
Nima Naghibi <dude.nimanaghibi@gmail.com>
2026-07-14 13:00:42 +0330

    feat: create a Set func for the Redis client

--- redis_cache.go
+++ redis_cache.go
@@ -19,6 +19,13 @@ func NewRedisCache(c *redis.Client) *RedisCache {
 
 func (c *RedisCache) Get(key int) (string, error) {
 	ctx := context.Background()
-	intStr := strconv.Itoa(key)
-	return c.client.Get(ctx, intStr).Result()
+	keyStr := strconv.Itoa(key)
+	return c.client.Get(ctx, keyStr).Result()
+}
+
+func (c *RedisCache) Set(key int, val string) error {
+	ctx := context.Background()
+	keyStr := strconv.Itoa(key)
+	_, err := c.client.Set(ctx, keyStr, val, 0).Result()
+	return err
 }
